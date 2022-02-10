4a742f8378421a04adac76c9d6d9737d74703b40
Termux DZ <95298894+Dhiyae@users.noreply.github.com>
2022-02-10 23:35:08 +0100

    Add files via upload

---
+++ metasploit-termux-dz.py
@@ -0,0 +1,32 @@
+import os,time
+os.system('clear')
+print('\033[1;33mwelcome ye my tools') 
+logo = """\033[1;31m
+      .:okOOOkdc'           'cdkOOOko:.
+    .xOOOOOOOOOOOOc       cOOOOOOOOOOOOx.
+   :OOOOOOOOOOOOOOOk,   ,kOOOOOOOOOOOOOOO:
+  'OOOOOOOOOkkkkOOOOO: :OOOOOOOOOOOOOOOOOO'
+  oOOOOOOOO.    .oOOOOoOOOOl.    ,OOOOOOOOo
+  dOOOOOOOO.      .cOOOOOc.      ,OOOOOOOOx
+  lOOOOOOOO.         ;d;         ,OOOOOOOOl
+  .OOOOOOOO.   .;           ;    ,OOOOOOOO.
+   cOOOOOOO.   .OOc.     'oOO.   ,OOOOOOOc
+    oOOOOOO.   .OOOO.   :OOOO.   ,OOOOOOo
+     lOOOOO.   .OOOO.   :OOOO.   ,OOOOOl
+      ;OOOO'   .OOOO.   :OOOO.   ;OOOO;
+       .dOOo   .OOOOocccxOOOO.   xOOd.
+         ,kOl  .OOOOOOOOOOOOO. .dOk,
+           :kk;.OOOOOOOOOOOOO.cOk:
+             ;kOOOOOOOOOOOOOOOk:
+               ,xOOOOOOOOOOOx,
+                 .lOOOOOOOl.
+                    ,dOd,
+                      .       """
+print(logo)
+time.sleep(4)
+os.system('pkg update -y;pkg upgrade -y;pkg install wget;wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh;chmod +x metasploit.sh;./metasploit.sh;msfconsole')
+
+
+
+
+
